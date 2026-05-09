882ec2b3190edf63c2209102a49c4670167554da
a8v0r6 <a8v0r6@protonmail.com>
2026-05-09 16:02:48 +0530

    MSG initiator

--- back-end/initiator/src/main/java/com/example/fix/initiator/config/InitiatorConfiguration.java
+++ back-end/initiator/src/main/java/com/example/fix/initiator/config/InitiatorConfiguration.java
@@ -2,14 +2,37 @@ package com.example.fix.initiator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import quickfix.DefaultMessageFactory;
-import quickfix.MessageFactory;
+import quickfix.*;
 
 @Configuration
 public class InitiatorConfiguration {
+
     @Bean
     public MessageFactory messageFactory() {
         // create messages in FIX protocol
         return new DefaultMessageFactory();
+    }
+
+    @Bean
+    public SessionSettings sessionSettings() throws ConfigError {
+        return new SessionSettings("quickfixj-initiator.cfg");
+    }
+
+    @Bean
+    public LogFactory logFactory() {
+        return new ScreenLogFactory();
+    }
+
+    @Bean
+    public MessageStoreFactory messageStoreFactory() {
+        return new MemoryStoreFactory();
+    }
+
+    @Bean
+    public Initiator initiator(Application application, MessageStoreFactory messageStoreFactory, SessionSettings sessionSettings, LogFactory logFactory, MessageFactory messageFactory) throws ConfigError {
+        SocketInitiator socketInitiator = new SocketInitiator(application, messageStoreFactory, sessionSettings, logFactory, messageFactory);
+        socketInitiator.start(); // single threaded
+        // use ThreadedSocketInitiator to handle multiple sessions
+        return socketInitiator;
     }
 }
---
+++ back-end/initiator/src/main/java/com/example/fix/initiator/dto/OrderDTO.java
@@ -0,0 +1,4 @@
+package com.example.fix.initiator.dto;
+
+public record OrderDTO(String symbol, Double quantity) {
+}
